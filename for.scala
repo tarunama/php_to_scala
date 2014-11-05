ac503bde27153dd612df07468d4cd409d8d90fab
meganeski <sakeharami@gmail.com>
2014-11-05 13:28:36 +0900

    add functional programing and assert

--- for.scala
+++ for.scala
@@ -5,8 +5,15 @@ $ary = count(args[1]);
 for ($i = 0; $i < $ary; $i++) {
     echo $ary[$i];
 }
-
 */
 
 for (arg <- args)
     println(arg)
+
+// これが関数型のプログラミングらしい
+def ArgsPrintln(args: Array[String]) = args.mkString("¥n")
+println(ArgsPrintln(args))
+
+// 評価も出来る
+val res = ArgsPrintln(args)
+assert(res == "hoge¥nfoo")
