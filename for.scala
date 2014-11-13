01c27fcb98a1d5afd685a2deaff8f5180927eae7
meganeski <sakeharami@gmail.com>
2014-11-14 00:45:58 +0900

    add filtering in for expression

--- for.scala
+++ for.scala
@@ -6,13 +6,22 @@ for ($i = 0; $i < $ary; $i++) {
     echo $ary[$i];
 }
 */
-
+// Scalaの場合
 for (arg <- args)
     println(arg)
 
 // これが関数型のプログラミングらしい
 def ArgsPrintln(args: Array[String]) = args.mkString("¥n")
 println(ArgsPrintln(args))
+
+// for式でフィルタリング
+val files = (new java.io.File(".")).listFiles
+// とてもしっくりこない
+for (
+	file <- files
+	if file.isFile
+	if file.getName.endsWith(".php")
+) println(file)
 
 // 評価も出来る
 val res = ArgsPrintln(args)
