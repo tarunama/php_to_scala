4b0c2bdb7e0af0ba28414d009983073cc2f8489b
meganeski <sakeharami@gmail.com>
2014-11-14 16:35:28 +0900

    add bind value in for statement

--- for.scala
+++ for.scala
@@ -14,15 +14,30 @@ for (arg <- args)
 def ArgsPrintln(args: Array[String]) = args.mkString("¥n")
 println(ArgsPrintln(args))
 
-// for式でフィルタリング
-val files = (new java.io.File(".")).listFiles
-// とてもしっくりこない
-for (
-	file <- files
-	if file.isFile
-	if file.getName.endsWith(".php")
-) println(file)
-
 // 評価も出来る
 val res = ArgsPrintln(args)
 assert(res == "hoge¥nfoo")
+
+// for式でフィルタリング
+val files = (new java.io.File(".")).listFiles
+// しっくりこない
+for (
+	file <- files
+	if file.isFile
+	if file.getName.endsWith(".php");
+) println(file)
+// 重複する計算はfor文内でbindさせる。セミコロンが邪魔なときは{}使う
+val filesHere = (new java.io.File(".")).listFiles
+
+def filelines(file: java.io.File) =
+	scala.io.Source.fromFile(file).getLines().toList
+// ここら辺不安
+def aryFor(args: String) =
+	for {
+		file <- filesHere
+		if file.isFile
+		// 重複部分をバインド
+		fileName = file.getName
+		if fileName.endsWith(".php")
+		if fileName.endsWith(".scala")	
+	} println(file)
