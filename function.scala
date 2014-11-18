096f702cff7cbe8b271f5b0f1d0149c9dcbdb758
meganeski <sakeharami@gmail.com>
2014-11-18 15:54:32 +0900

    add apply method and parameter etc

--- function.scala
+++ function.scala
@@ -10,3 +10,12 @@ def add (x: Int, y: Int): Int = {
     x + y
 }
 add(1, 2)
+
+// 上のコードはapplyメソッドが省略されている。
+add.apply(1, 2)
+
+//　パラメーターを直接指定できる
+def echo (a: String, b: String) = {
+    println(a + b)
+}
+echo(b = "bbbb", a = "aaa") //　あまり使う機会を思いつかない
