d5e0e3771c697b53147482e0aa8ee274ab298a2d
meganeski <sakeharami@gmail.com>
2014-11-18 14:34:02 +0900

    first_commit

---
+++ method.scala
@@ -0,0 +1,15 @@
+val nums = List(11, 20, 4, -9, 0, 6)
+nums.foreach((x:Int) => println(x))
+
+//　型推論してくれるので型を省ける
+nums.foreach((x) => println(x))
+
+// ()もいらない
+nums.foreach(x => println(x))
+
+// この場合はこれでいい
+nums.foreach(println)
+
+// プレースホルダーを使うと
+nums.foreach(println(_))
+
